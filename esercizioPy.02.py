58fe777605ef45d9a8518d83d6dc4a54c6efbbf2
davidelastrucci <74978998+davidelastrucci@users.noreply.github.com>
2020-12-09 18:55:51 +0100

    Add files via upload

---
+++ esercizioPy.02.py
@@ -0,0 +1,24 @@
+a = int(input("qual'è il valore di a?"))
+b = int(input("qual'è il valore di b?"))
+x = (-b/a)
+print("il valore di x è",x)
+
+if a>b:
+    if a>x:
+        max = a
+    else:
+        max = x
+else:
+    if b>x:
+        max = b
+    else:
+        max = x
+print("il maggiore tra",a,",",b,"e",x,"è",max)
+
+a2 = a
+b2 = b
+def swap(inversioneab):
+    a == b2
+    b == a2
+    return a,b
+print(swap)
