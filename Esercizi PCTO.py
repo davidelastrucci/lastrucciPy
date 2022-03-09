e87c1ef03ae9a448f6447bd9ecdd3c31150a40cc
davidelastrucci <74978998+davidelastrucci@users.noreply.github.com>
2022-03-09 16:42:13 +0100

    Add files via upload

---
+++ Esercizi PCTO.py
@@ -0,0 +1,86 @@
+#lista_Giochi = ['Minecraft', 'Gta', 'PacMan', 'CandyCrush', 'Rocket'] 
+#for indice_Giochi in range(len(lista_Giochi)):
+    #print('indice corrente:', indice_Giochi, "Gioco corrente", lista_Giochi[indice_Giochi])
+
+#spese = [1, 5, 7, 9]
+#somma = 0
+#for spesa in spese:
+    #somma += spesa
+#print('Hai speso:', somma)
+
+#lista_citta = ['Napoli', 'Roma', 'Milano', 'Bari']
+#lista_citta[0], lista_citta[1], lista_citta[1], lista_citta [0]
+#print(lista_citta)
+
+#lista_citta.sort(reverse=True)
+#print (lista_citta)
+
+#numeri = []
+#for i in range (1,101):
+    #numeri.append(i)
+#print(numeri)
+
+#numeri = [i for i in range (1,101)]
+#print(numeri)
+
+#numeri = [i for i in range (1,101) if i % 3 != 0]
+#print (numeri)
+
+#numeri = [1,2,3,4,5]
+#paesi = ['Italia', 'Inghilterra']
+#print
+
+#celle =[['A1', 'A2', 'A3'] ['B1', 'B2', 'B3']]
+#for x in celle:
+    #for y in x:
+       #print('elemento:', y)
+
+#lista =['A1', 'A2', 'A3']
+#print (celle[0][0])
+
+#tabella =[[i for i in range(1,6)] for j in range (4)]
+
+#lista1 = ['Roma', 'Napoli']
+#lista2 = ['Milano', 'Bari']
+#lista_somma = lista1 + lista2
+#print(lista_somma)
+
+#lista_numeri = [0,1] * 10
+#print(lista_numeri)
+
+#nome = 'Davide'
+#print(nome[4])
+
+#testo = 'rendimi maiuscolo'
+#testo_maiuscolo = testo.upper()
+#print(testo_maiuscolo)
+
+#numero_inserito = input('Per favore inserisci un numero:')
+#if numero_inserito.isnumeric():
+    #print ('Grazie')
+#else:
+    #print ('Mi dispiace non è un numero')
+
+#dizionario = {}
+#tuple = ()
+#lista = []
+#nome = ''
+
+#tuple_a = (1,2)
+#tuple_b = 1, 2
+
+#dati_utente = ('Antonio', 'esposito', '1992') + ('perditempo')
+#print(dati_utente)
+
+#capitali = [('Londra' , 'UK', 8.98), ('Roma', 'It', 1.0)]
+#for capitale in capitali:
+    #print('Citta:', capitale[0], 'Paese:', capitale [1], 'Popolazione:', capitale [2])
+
+#emails = {
+  #  'Antonio': 'antonio@gmail.com'
+#}
+#print(emails['Antonio'])
+
+
+
+
